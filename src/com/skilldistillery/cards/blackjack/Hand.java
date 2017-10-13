56f9724016e1db652548f26b42456fda7e273903
Jacob Simms <simmsjt@Jacobs-MacBook-Pro.local>
2017-10-13 15:47:44 -0600

    updated hand class

--- src/com/skilldistillery/cards/blackjack/Hand.java
+++ src/com/skilldistillery/cards/blackjack/Hand.java
@@ -4,14 +4,28 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Hand {
-	private List <Card> hand;
-	
+	private List<Card> hand;
+
 	public Hand(ArrayList<Card> hand) {
 		this.hand = hand;
 	}
-	
+
 	public List<Card> getHand() {
 		return hand;
 	}
-	
+
+	public void addCard(Card c) {
+		hand.add(c);
+	}
+
+	public void clearHand() {
+		hand.clear();
+	}
+
+	public void printHand() {
+		for (int x = 0; x < hand.size(); x++) {
+			System.out.println(hand.get(x).getRank() + " of " + hand.get(x).getSuit());
+		}
+	}
+
 }
