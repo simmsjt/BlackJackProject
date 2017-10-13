dd6f8d5f3efbd7b33f398913b0d3f05804353042
Jacob Simms <simmsjt@Jacobs-MacBook-Pro.local>
2017-10-13 17:28:34 -0600

    added char values to Rank class

--- src/com/skilldistillery/cards/blackjack/Rank.java
+++ src/com/skilldistillery/cards/blackjack/Rank.java
@@ -2,23 +2,25 @@ package com.skilldistillery.cards.blackjack;
 
 public enum Rank {
 
-    TWO(2), 
-    THREE(3), 
-    FOUR(4), 
-    FIVE(5), 
-    SIX(6), 
-    SEVEN(7), 
-    EIGHT(8), 
-    NINE(9),
-    TEN(10),
-    JACK(10),
-    QUEEN(10), 
-    KING(10), 
-    ACE(11);
+    TWO(2,'2'), 
+    THREE(3,'3'), 
+    FOUR(4,'4'), 
+    FIVE(5,'5'), 
+    SIX(6,'6'), 
+    SEVEN(7,'7'), 
+    EIGHT(8,'8'), 
+    NINE(9,'9'),
+    TEN(10,'T'),
+    JACK(10,'J'),
+    QUEEN(10,'Q'), 
+    KING(10,'K'), 
+    ACE(11,'A');
 	
 	public final int value;
+	public final char symbol;
 	
-	private Rank(int value) {
+	private Rank(int value, char symbol) {
+		this.symbol = symbol;
 		this.value = value;
 	}
 
@@ -26,5 +28,8 @@ public enum Rank {
 		return value;
 	}
 	
+	public char getSymbol() {
+		return symbol;
+	}
 	
 }
