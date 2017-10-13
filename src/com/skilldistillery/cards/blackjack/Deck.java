341cc3061e937710b0d0c112e548592f4759bf33
Jacob Simms <simmsjt@Jacobs-MacBook-Pro.local>
2017-10-13 16:09:37 -0600

    added methods to Deck class

--- src/com/skilldistillery/cards/blackjack/Deck.java
+++ src/com/skilldistillery/cards/blackjack/Deck.java
@@ -1,5 +1,55 @@
 package com.skilldistillery.cards.blackjack;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class Deck {
+	private List<Card> deck;
+
+	public Deck() {
+		generateDeck();
+	}
+
+	public Deck(List<Card> deck) {
+		this.deck = deck;
+	}
+	
+	
+	private void generateDeck() {
+		deck = new ArrayList<>(52);
+		for (Suit s : Suit.values()) {
+			for (Rank r : Rank.values()) {
+				deck.add(new Card(r, s));
+			}
+		}
+	}
+	
+	public Card drawCard() {
+		return deck.remove(0);
+	}
+	
+	public void addCard(Card c) {
+		deck.add(c);
+	}
+	
+	public void printCards(){
+		for (int x = 0; x < deck.size(); x++) {
+			System.out.println(deck.get(x).getRank() + " of " + hand.get(x).getSuit());
+		}
+	}
+	
+	public void shuffle() {
+		Collections.shuffle(deck);
+	}
+
+	public List<Card> getDeck() {
+		return deck;
+	}
+
+	public void setDeck(List<Card> deck) {
+		this.deck = deck;
+	}
+	
 
 }
