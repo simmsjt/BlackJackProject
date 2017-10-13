6bf79f20c5dec02741b6a6c11e30befa20f53ea6
Jacob Simms <simmsjt@Jacobs-MacBook-Pro.local>
2017-10-13 16:52:40 -0600

    added methods to player class

---
+++ src/com/skilldistillery/cards/blackjack/Casino.java
@@ -0,0 +1,8 @@
+package com.skilldistillery.cards.blackjack;
+
+public class Casino {
+	//driver class
+	public static void main(String[] args) {
+		
+	}
+}
--- src/com/skilldistillery/cards/blackjack/Player.java
+++ src/com/skilldistillery/cards/blackjack/Player.java
@@ -1,5 +1,59 @@
 package com.skilldistillery.cards.blackjack;
 
 public class Player {
+	
+	private Hand hand;
+	private String name;
+	private int totalMoney;
+	
+	
+	
+	public Player() {
+		setTotalMoney(0);
+	}
+	
+	
+	public Player(String name) {
+		setName(name);
+		setTotalMoney(0);
+	}
+	
+	
+	public Player(Hand hand) {
+		setHand(hand);
+		setTotalMoney(0);
+	}
+	
+	public Player(Hand hand, String name) {
+		setName(name);
+		setHand(hand);
+		setTotalMoney(0);
+	}
+	
+	
 
+	public Hand getHand() {
+		return hand;
+	}
+
+	public void setHand(Hand hand) {
+		this.hand = hand;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getTotalMoney() {
+		return totalMoney;
+	}
+
+	public void setTotalMoney(int totalMoney) {
+		this.totalMoney = totalMoney;
+	}
+	
 }
